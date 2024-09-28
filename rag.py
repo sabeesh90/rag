dc87a1aa32ae94b22b5c15eb07a6afde593bddb6
Sabeesh E <sabeesh90@yahoo.co.uk>
2024-09-28 16:24:03 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -123,6 +123,7 @@ if prompt := st.chat_input("Hei Sabeesh!"):
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         # format_output(response)
+        st.write(response)
         st.markdown(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
